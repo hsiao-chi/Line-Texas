4bd0566cc31cf5b313e215630326456351d06ac0
garylai00000 <garylai00000@gmail.com>
2016-07-28 15:16:36 +0800

    u

--- main.go
+++ main.go
@@ -75,7 +75,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 			prof,_ := bot.GetUserProfile([]string{"ubea7d66dbde55879bcd1d492cae2bb1b"})
 			qq := prof.Contacts
 			db,_ := sql.Open("mysql", "database1234:Tg7y-Bx!ow8z@tcp(mysql3.gear.host:3306)/")
-			db.Exec("INSERT INTO database1234.linebot VALUES (?, ?, ?)", content.From, qq.DisplayName, text.Text)
+			db.Exec("INSERT INTO database1234.linebot VALUES (?, ?, ?)", content.From, qq.MID, text.Text)
 			db.Close()
 		}
 	}
