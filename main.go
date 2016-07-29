7b8ae3307ab3164140e85d39fffbbfdb2419c18d
garylai00000 <garylai00000@gmail.com>
2016-07-29 11:11:58 +0800

    s

--- main.go
+++ main.go
@@ -73,9 +73,9 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 			_, err = bot.SendText([]string{"ubea7d66dbde55879bcd1d492cae2bb1b"}, text.Text) // sent to garylai
 			
 			prof,_ := bot.GetUserProfile([]string{content.From})
-			//qq := prof.Contacts
+			info := prof.Contacts
 			db,_ := sql.Open("mysql", "database1234:Tg7y-Bx!ow8z@tcp(mysql3.gear.host:3306)/")
-			db.Exec("INSERT INTO database1234.linebot VALUES (?, ?, ?)", content.From, prof.Count, text.Text)
+			db.Exec("INSERT INTO database1234.linebot VALUES (?, ?, ?)", content.From, info[0].DisplayName, text.Text)
 			db.Close()
 		}
 	}
