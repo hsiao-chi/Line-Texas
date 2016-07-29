0f4f52598392e127b8c9df4110ee4b898944465a
garylai00000 <garylai00000@gmail.com>
2016-07-29 16:20:10 +0800

    s

--- main.go
+++ main.go
@@ -75,7 +75,8 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 			
 			
 			db,_ := sql.Open("mysql", "database1234:Tg7y-Bx!ow8z@tcp(mysql3.gear.host:3306)/")
-			db.Exec("INSERT INTO database1234.linebottext VALUES (?, ?, ?)", info[0].MID, info[0].DisplayName, text.Text)
+			db.Exec("INSERT INTO database1234.linebottext(mid, text) VALUES (?, ?)", info[0].MID,  text.Text)
+			//db.Exec("INSERT INTO database1234.linebottext VALUES (?, ?, ?)", info[0].MID, info[0].DisplayName, text.Text)
 			db.Close()
 		}
 	}
