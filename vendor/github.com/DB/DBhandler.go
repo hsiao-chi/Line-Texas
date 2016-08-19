b397e719e4509baf34264812607540de9d5abeed
YozoraSS <h39y21c74@gmail.com>
2016-08-20 02:07:24 +0800

    Update DBhandler.go
    加入 InRoom的三個func

--- vendor/github.com/DB/DBhandler.go
+++ vendor/github.com/DB/DBhandler.go
@@ -3,8 +3,10 @@
 他手上有哪兩張牌
 */
 package DB
-import(
+import (
 	"os"
+	"strconv"
+	"github.com/line/line-bot-sdk-go/linebot"
 	"database/sql"
 	_"github.com/go-sql-driver/mysql"
 )
@@ -27,6 +29,8 @@ func UserGamming(MID string) bool{
 
 }
 func InRoomInst(MID string){
+	strID := os.Getenv("ChannelID")
+	numID, _ := strconv.ParseInt(strID, 10, 64) // string to integer
 	bot, _ = linebot.NewClient(numID, os.Getenv("ChannelSecret"), os.Getenv("MID"))
 	db,_ := sql.Open("mysql", os.Getenv("dbacc")+":"+os.Getenv("dbpass")+"@tcp("+os.Getenv("dbserver")+")/")
 	var haveGame string
@@ -43,6 +47,8 @@ func InRoomInst(MID string){
 	db.Close()
 }
 func InRoomNewGame(MID string){
+	strID := os.Getenv("ChannelID")
+	numID, _ := strconv.ParseInt(strID, 10, 64) // string to integer
 	bot, _ = linebot.NewClient(numID, os.Getenv("ChannelSecret"), os.Getenv("MID"))
 	db,_ := sql.Open("mysql", os.Getenv("dbacc")+":"+os.Getenv("dbpass")+"@tcp("+os.Getenv("dbserver")+")/")
 	var haveGame string
@@ -52,14 +58,16 @@ func InRoomNewGame(MID string){
 	db.QueryRow("SELECT ID FROM sql6131889.Room WHERE  RoomName = ?", R).Scan(&RID)
 	db.QueryRow("SELECT RoomID FROM sql6131889.Game WHERE RoomID = ?", RID).Scan(&haveGame)
 	if haveGame == ""{
-		db.Exec("INSERT INTO sql6131889.Game (GameName, RoomID, GameStatus, GameTokens, GamePlayer1, GameMaster, Cancel) VALUES (?, ?, ?, ?, ?, ?, ?)", "TexasPoker", RID, 100, 0, info[0].MID, "0", 0)
-		bot.SendText([]string{content.From}, "You created a new game")
+		db.Exec("INSERT INTO sql6131889.Game (GameName, RoomID, GameStatus, GameTokens, GamePlayer1, GameMaster, Cancel) VALUES (?, ?, ?, ?, ?, ?, ?)", "TexasPoker", RID, 100, 0, MID, "0", 0)
+		bot.SendText([]string{MID}, "You created a new game")
 	}else{
-		bot.SendText([]string{content.From}, "There is already a game in this room!!")
+		bot.SendText([]string{MID}, "There is already a game in this room!!")
 	}
 	db.Close()
 }
 func InRoomJoinGame(MID string){
+	strID := os.Getenv("ChannelID")
+	numID, _ := strconv.ParseInt(strID, 10, 64) // string to integer
 	bot, _ = linebot.NewClient(numID, os.Getenv("ChannelSecret"), os.Getenv("MID"))
 	db,_ := sql.Open("mysql", os.Getenv("dbacc")+":"+os.Getenv("dbpass")+"@tcp("+os.Getenv("dbserver")+")/")
 	var haveGame string
@@ -69,7 +77,7 @@ func InRoomJoinGame(MID string){
 	db.QueryRow("SELECT ID FROM sql6131889.Room WHERE  RoomName = ?", R).Scan(&RID)
 	db.QueryRow("SELECT RoomID FROM sql6131889.Game WHERE RoomID = ?", RID).Scan(&haveGame)
 	if haveGame == ""{
-		bot.SendText([]string{content.From}, "Please create a new game")
+		bot.SendText([]string{MID}, "Please create a new game")
 	}else{
 		db.Exec("UPDATE sql6131889.Game SET UserStatus = ? WHERE MID = ?", 10, MID)
 	}
