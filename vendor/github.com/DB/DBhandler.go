142619d9b2cd9fa52e8358e0ba43fc38765f1a5f
YozoraSS <h39y21c74@gmail.com>
2016-08-20 04:42:50 +0800

    Update DBhandler.go

--- vendor/github.com/DB/DBhandler.go
+++ vendor/github.com/DB/DBhandler.go
@@ -5,8 +5,6 @@
 package DB
 import (
 	"os"
-	"strconv"
-	"github.com/line/line-bot-sdk-go/linebot"
 	"database/sql"
 	_"github.com/go-sql-driver/mysql"
 )
@@ -14,7 +12,6 @@ import (
 傳入使用者MID
 回傳使用者是否正在遊戲
 */
-var bot *linebot.Client
 func UserGamming(MID string) bool{
 	var GameID int
 	GameID = 0;
@@ -27,59 +24,4 @@ func UserGamming(MID string) bool{
 	}
 
 
-}
-func InRoomInst(MID string){
-	strID := os.Getenv("ChannelID")
-	numID, _ := strconv.ParseInt(strID, 10, 64) // string to integer
-	bot, _ = linebot.NewClient(numID, os.Getenv("ChannelSecret"), os.Getenv("MID"))
-	db,_ := sql.Open("mysql", os.Getenv("dbacc")+":"+os.Getenv("dbpass")+"@tcp("+os.Getenv("dbserver")+")/")
-	var haveGame string
-	var RID string
-	var R string
-	db.QueryRow("SELECT UserRoom FROM sql6131889.User WHERE MID = ?", MID).Scan(&R)
-	db.QueryRow("SELECT ID FROM sql6131889.User WHERE  RoomName = ?", R).Scan(&RID)
-	db.QueryRow("SELECT RoomID FROM sql6131889.Game WHERE RoomID = ?", RID).Scan(&haveGame)
-	if haveGame == ""{
-		bot.SendText([]string{MID}, "You can use these instruction:\n!leavechatroom\n!newgame")
-	}else{
-		bot.SendText([]string{MID}, "You can use these instruction:\n!leavechatroom")
-	}
-	db.Close()
-}
-func InRoomNewGame(MID string){
-	strID := os.Getenv("ChannelID")
-	numID, _ := strconv.ParseInt(strID, 10, 64) // string to integer
-	bot, _ = linebot.NewClient(numID, os.Getenv("ChannelSecret"), os.Getenv("MID"))
-	db,_ := sql.Open("mysql", os.Getenv("dbacc")+":"+os.Getenv("dbpass")+"@tcp("+os.Getenv("dbserver")+")/")
-	var haveGame string
-	var RID string
-	var R string
-	db.QueryRow("SELECT UserRoom FROM sql6131889.User WHERE MID = ?", MID).Scan(&R)
-	db.QueryRow("SELECT ID FROM sql6131889.Room WHERE  RoomName = ?", R).Scan(&RID)
-	db.QueryRow("SELECT RoomID FROM sql6131889.Game WHERE RoomID = ?", RID).Scan(&haveGame)
-	if haveGame == ""{
-		db.Exec("INSERT INTO sql6131889.Game (GameName, RoomID, GameStatus, GameTokens, GamePlayer1, GameMaster, Cancel) VALUES (?, ?, ?, ?, ?, ?, ?)", "TexasPoker", RID, 100, 0, MID, "0", 0)
-		bot.SendText([]string{MID}, "You created a new game")
-	}else{
-		bot.SendText([]string{MID}, "There is already a game in this room!!")
-	}
-	db.Close()
-}
-func InRoomJoinGame(MID string){
-	strID := os.Getenv("ChannelID")
-	numID, _ := strconv.ParseInt(strID, 10, 64) // string to integer
-	bot, _ = linebot.NewClient(numID, os.Getenv("ChannelSecret"), os.Getenv("MID"))
-	db,_ := sql.Open("mysql", os.Getenv("dbacc")+":"+os.Getenv("dbpass")+"@tcp("+os.Getenv("dbserver")+")/")
-	var haveGame string
-	var RID string
-	var R string
-	db.QueryRow("SELECT UserRoom FROM sql6131889.User WHERE MID = ?", MID).Scan(&R)
-	db.QueryRow("SELECT ID FROM sql6131889.Room WHERE  RoomName = ?", R).Scan(&RID)
-	db.QueryRow("SELECT RoomID FROM sql6131889.Game WHERE RoomID = ?", RID).Scan(&haveGame)
-	if haveGame == ""{
-		bot.SendText([]string{MID}, "Please create a new game")
-	}else{
-		db.Exec("UPDATE sql6131889.Game SET UserStatus = ? WHERE MID = ?", 10, MID)
-	}
-	db.Close()
 }
