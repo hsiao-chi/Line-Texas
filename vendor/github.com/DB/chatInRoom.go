55ed51e64b4b247eb241b5231ac840f27fcf094b
hsiao-chi <ciyalnin@gmail.com>
2016-08-20 19:50:46 +0800

    Update chatInRoom.go

--- vendor/github.com/DB/chatInRoom.go
+++ vendor/github.com/DB/chatInRoom.go
@@ -55,17 +55,20 @@ func Management(mID string, text string) { // if playing call this func
 			bot.SendText([]string{mid1}, "現在開始遊戲-Texas")
 		}
 		db.Exec("UPDATE sql6131889.Game SET GameStatus = ? WHERE RoomId = ?",3,gID)
+		S=3
 	}
 	if S == 3{//發牌=一人2張
 		row,_ := db.Query("SELECT MID FROM sql6131889.GameAction WHERE GameID = ?", gID)
+		i:=0
 		for row.Next() {
+			i++
 			var mid1 string
 			row.Scan(&mid1)
 			var cards [2]int
 			cards = GetTwoCards(mid1)
 			c1 := GetCardName(cards[0])
 			c2 := GetCardName(cards[1])
-			bot.SendText([]string{mid1}, "您的手牌為：\n" + c1 + "\n" + c2)
+			bot.SendText([]string{mid1}, "您的手牌為：\n" + c1 + "\n" + c2+strconv.Itoa(i))
 		}
 		var p1 string
 		db.QueryRow("SELECT MID FROM sql6131889.GameAction WHERE PlayerX = ?AND GameID = ?",1,gID).Scan(&p1)
